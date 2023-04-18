fcfe9dda2f078cd8a81dfc19939b68a4567f1c9b
Josschua <66189800+Josschua@users.noreply.github.com>
2023-04-18 01:01:21 -0400

    Delete MagneticSensor.java

--- MagneticSensor.java
+++
@@ -1,9 +0,0 @@
-public class MagneticSensor extends Sensor {
-    public MagneticSensor(){}
-    public void moveMagnetAwayFromSwitch() {
-        setState(SwitchState.OPEN);
-    }
-    public void putMagnetNearSwitch() {
-        setState(SwitchState.CLOSE);
-    }
-}
